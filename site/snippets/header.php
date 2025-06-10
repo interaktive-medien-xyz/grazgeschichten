130c8b25b540e923f6b6d0f31ba325f5276fe1d7
WinklerOstfalia <mor.winkler@ostfalia.de>
2025-06-10 14:15:15 +0200

    Scroll-Slider-Alternative hinzugefügt

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -55,33 +55,46 @@
 </head>
 <body>
 
-<?php /*
   <header class="header">
-    <?php 
-
+    <?php
+    /*
+      We use `$site->url()` to create a link back to the homepage
+      for the logo and `$site->title()` as a temporary logo. You
+      probably want to replace this with an SVG.
+    */
     ?>
     <a class="logo" href="<?= $site->url() ?>">
       <?= $site->title()->esc() ?>
     </a>
 
     <nav class="menu">
+      <?php
+      /*
+        In the menu, we only fetch listed pages,
+        i.e. the pages that have a prepended number
+        in their foldername.
 
+        We do not want to display links to unlisted
+        `error`, `home`, or `sandbox` pages.
+
+        More about page status:
+        https://getkirby.com/docs/reference/panel/blueprints/page#statuses
+      */
+      ?>
       <?php foreach ($site->children()->listed() as $item): ?>
       <a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a>
       <?php endforeach ?>
       <?php snippet('social') ?>
     </nav>
-  </header> 
-  */
-  ?>  
+  </header>
 
   <header class="home-header">
   <a href="<?= $site->url() ?>" class="logo">
   <h1 class="logo-graz">GRAZ</h1>
   <h1 class="logo-geschichten">GESCHICHTEN</h1>
-  </a> 
+  </a>
 
-  <a class="about-link" href="about">Über das Projekt</a>
+  <a class="about-link" href="">Über das Projekt</a>
 </header>
 
   <main class="main">
